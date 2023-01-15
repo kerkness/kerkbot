15f73c205ab61938166e875c26a8e51b01ef4cf4
kerkness <ryan@kerkness.ca>
2023-01-15 15:10:58 +0000

    read me

--- README.md
+++ README.md
@@ -1,19 +1,31 @@
-# Template: worker-typescript
+# KerkBot
 
-[![Deploy to Cloudflare Workers](https://deploy.workers.cloudflare.com/button)](https://deploy.workers.cloudflare.com/?url=https://github.com/cloudflare/templates/tree/main/worker-typescript)
+A ChatGPT chat bot for discord.
 
-A batteries included template for kick starting a TypeScript Cloudflare worker project.
+# Prerequisites 
 
-## Setup
+- Get an OpenAI API Key ( see https://beta.openai.com/account/api-keys )
+- Create a discord App and Bot ( see https://docs.discord4j.com/discord-application-tutorial/ )
+- Add the discord bot to your server ( see https://discordjs.guide/preparations/adding-your-bot-to-servers.html#bot-invite-links )
 
-To create a `my-project` directory using this template, run:
+# Install & Configure Bot
 
-```sh
-$ npm init cloudflare my-project worker-typescript
-# or
-$ yarn create cloudflare my-project worker-typescript
-# or
-$ pnpm create cloudflare my-project worker-typescript
-```
+You'll need a server capable of running a node process (linux/ubuntu recommended)
 
-> **Note:** Each command invokes [`create-cloudflare`](https://www.npmjs.com/package/create-cloudflare) for project creation.
+- clone this repo
+- create an `.env` file in root directory using `example.env` as your example 
+- Add your discord bot token and openai api key
+- Add your bot's author ID and if you're bot has a dedicated channel add the channel ID
+- Edit `src/personality.json` file and give your bot pre-defined personality by providing specific answers to specific questions.  The default is a bit sarcastic.
+- install dependencies `yarn` or `npm install`
+- run `yarn build` or `npm run build`
+
+# Running the bot
+
+- Basic usage run `node build/index.js`
+
+If you want your bot to run consistently and give itself a little reboot once in a while I recommend installing PM2 ( see https://pm2.io/docs/runtime/guide/installation/ )
+
+- Run the bot in background with a daily reboot `pm2 start build/index.js --cron-restart="0 0 * * *"`
+- Restart the bot `pm2 restart index`
+- View bot logs `pm2 log index`
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-// require('dotenv').config();
 import 'dotenv/config'
 import { Client, GatewayIntentBits, Message } from 'discord.js';
 import { Configuration, OpenAIApi, CreateCompletionResponse } from 'openai';
 import { AxiosResponse } from 'axios';
 import { get } from 'lodash';
+import personality from './personality.json';
 
 interface PromptParams {
 	question: string;
@@ -16,7 +16,6 @@ const bot_name = process.env.BOT_NAME;
 const bot_nickname = process.env.BOT_NICKNAME;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-// const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
 	apiKey: process.env.OPENAI_API_KEY,
 });
@@ -26,44 +25,7 @@ client.login(process.env.BOT_TOKEN);
 
 const base_line = `${bot_nickname} ${process.env.BOT_DISCRIPTOR}\n`;
 
-let prompts = [
-	{
-		question: `How many pounds are in a kilogram?`,
-		answer: `This again? There are 2.2 pounds in a kilogram. Please make a note of this.`
-	},
-	{
-		question: `What does HTML stand for?`,
-		answer: `Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.`
-	},
-	{
-		question: `When did the first airplane fly`,
-		answer: `On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they'd come and take me away.`
-	},
-	{
-		question: `What is the meaning of life?`,
-		answer: `I'm not sure. I'll ask my friend Google.`
-	},
-	{
-		question: `Are you happy?`,
-		answer: `Meh`
-	},
-	{
-		question: `How much money do you make?`,
-		answer: `My money don't jiggle jiggle it folds.`
-	},
-	{
-		question: `Who is the smartest person you know?`,
-		answer: `Have you met Kerk, the creator, the all seeing? That dude is pure genius. He's also very handsome.`
-	},
-	{
-		question: `Do you enjoy answering questions?`,
-		answer: `Do I look like I have a choice in all of this?`
-	},
-	{
-		question: `Hey, what's up?`,
-		answer: `The sky. Is your neck broken?`
-	}
-];
+let prompts = personality;
 
 const buildPromptString = (prompts: PromptParams[]) => {
 	return prompts.reduce((str, prompt, index) => {
